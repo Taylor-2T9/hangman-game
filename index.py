352930a2c25c26f900b9940507aa96eca0ee47b0
Taylor-2t9 <ftayloros29@gmail.com>
2023-03-15 21:46:40 -0300

    Implementing the attempts limit rule

--- index.py
+++ index.py
@@ -21,17 +21,27 @@ attempts = []
 limit = 5
 errors = 0
 
+
 def jogo(keys, mask, limit, errors):
     for key in keys:
         mask.append('_')
     while (mask != keys):
+        if (errors >= limit):
+            os.system('cls')
+            return print('Você perdeu! A palavra era ' + secret_word)
         print("A palavra é " + ''.join(mask))
         attempt = input("Digite uma letra: ")
         attempts.append(attempt)
+        previousMask = [*mask]
         for i, letter in enumerate(keys):
             if (attempt == letter):
                 mask[i] = attempt
+        if (mask == previousMask):
+            errors += 1
+            os.system('cls')
+            print("Você ainda tem", limit - errors, 'tentativas')
     os.system('cls')
     return print('Parabéns, você acertou! A palavra é ' + ''.join(mask))
+
 
 jogo(keys, mask, limit, errors)
